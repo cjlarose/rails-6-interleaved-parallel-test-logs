01997529f59e433df35c4521b6d116a5fe6e4166
Chris LaRose <cjlarose@gmail.com>
2019-10-04 15:19:01 -0700

    Add the ability to customize parallelization model with env var

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: 2)
+  parallelize(workers: 2, with: ENV.fetch('PARALLELIZE_WITH', 'processes').to_sym)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
